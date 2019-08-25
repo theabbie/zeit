3f19e304c2a2e5664efda5c36fea69a540dd5c78
abhishek <abhishek7gg7@gmail.com>
2019-08-25 16:40:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var axios = require('axios');
 
-io.on('connection', function(socket){
-  socket.on('message', function(msg){
-    io.emit('message', msg);
-  });
-});
+app.get("/*", function(req,res) {
+axios.get(faker.image.avatar(), function(img) {
+res.send(img)
+})
+})
 
-http.listen(process.env.PORT);
