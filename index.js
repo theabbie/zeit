f49b13064f9762dd89d2a82ce9214805880f3484
abhishek <abhishek7gg7@gmail.com>
2019-08-25 13:51:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,31 @@ const app = express();
 const fs = require('fs');
 const formidable = require('formidable');
 const store = require("data-store");
-var db = new store({path: "/data.json"})
+var db = new store({path: "/tmp/data.json"})
 
 app.post('/upload', function(req, res) {
 var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
     db.set("name",files.file.name)
-      fs.rename(files.file.path, "/"+files.file.name, function (err) {
+      fs.rename(files.file.path, "/tmp/"+files.file.name, function (err) {
         if (err) throw err;
-        res.redirect(301,"https://srvrr.tk/custom/file");
+        res.redirect(301,"https://filed.now.sh/hmm");
       });
  });
 });
+
+app.get('/', function(req, res) {
+res.setHeader("content-type","text/html");
+res.end(`
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<form method="post" action="https://filed.now.sh/upload" enctype="multipart/form-data">
+<input type="file" name="file">
+<input type="submit">
+</form>
+</html>
+`);
+})
 
 app.get('/*', function(req, res) {
 res.sendFile('/'+db.get("name"));
