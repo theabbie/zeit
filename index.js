15b555ce596307541a7f26066ed2e7ccd7fa7916
abhishek <abhishek7gg7@gmail.com>
2019-08-25 16:04:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var app = require('express')();
-var http = require('http').createServer(app);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+io.on('connection', function(socket){
+  socket.on('message', function(msg){
+    io.emit('message', msg);
+  });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+http.listen(process.env.PORT);
