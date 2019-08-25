94546b4111ef71434ced6330aa5c3d82c0e1725d
abhishek <abhishek7gg7@gmail.com>
2019-08-25 15:06:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 var io = require('socket.io').listen(80);
  
 io.sockets.on('connection', function (socket) {
-  console.log("Someone just connected!");
-  
-  // Echo back messages from the client
   socket.on('message', function (message) {
-    console.log("Got message: " + message);
     socket.emit('message', message);
   });  
 });
