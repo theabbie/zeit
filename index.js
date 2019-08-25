30273a2271c80ae7f2604c44e4106201dcc898ea
abhishek <abhishek7gg7@gmail.com>
2019-08-25 19:11:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ var axios = require('axios');
 var faker = require('faker')
 
 app.get("/*", function(req,res) {
-axios.get("https://typi.tk/?url=https://medium.com/&sel=a&attribs=href&static=true", function(links) {
+axios.get("https://typi.tk/?url=https://medium.com/&sel=a&attribs=href&static=true").then(function(links) {
 var result = "";
 for (x in links) {
-result+="<a href='"+links[x]+"'>"+links[x]+"</a>"
+result+="<a href='"+links[x]+"'>"+links[x]+"</a><br>"
 }
+res.setHeader("content-type","text/html");
 res.end(result);
 })
 })
