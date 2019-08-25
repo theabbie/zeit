a70ea9b6f30b1996828fed7795d242ff9dfdf114
abhishek <abhishek7gg7@gmail.com>
2019-08-25 22:21:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -206,7 +206,6 @@ ${req.url=="/"?"":extra}
 
 <!-- Footer -->
 <footer class="w3-container w3-dark-grey w3-padding-32 w3-margin-top">
-  <button class="w3-button w3-black w3-disabled w3-padding-large w3-margin-bottom">Previous</button>
   <button class="w3-button w3-black w3-padding-large w3-margin-bottom" onclick="location.reload()">Next &raquo;</button>
   <p class="w3-text-white w3-large">PAIGE</p>
 </footer>
