6021897c4896dc8c953111f0bb789f5d404680c2
abhishek <abhishek7gg7@gmail.com>
2019-08-25 15:02:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,11 @@
-const express = require('express');
-const app = express();
-const fs = require('fs');
-const formidable = require('formidable');
-const store = require("data-store");
-var db = new store({path: "/tmp/data.json"})
-
-app.post('/upload', function(req, res) {
-var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-    db.set("name",files.file.name)
-      fs.rename(files.file.path, "/tmp/"+files.file.name, function (err) {
-        if (err) throw err;
-        res.redirect(301,"https://filed.now.sh/"+Math.floor(Math.random()*100));
-      });
- });
+var io = require('socket.io').listen(80);
+ 
+io.sockets.on('connection', function (socket) {
+  console.log("Someone just connected!");
+  
+  // Echo back messages from the client
+  socket.on('message', function (message) {
+    console.log("Got message: " + message);
+    socket.emit('message', message);
+  });  
 });
-
-app.get('/', function(req, res) {
-res.setHeader("content-type","text/html");
-res.end(`
-<html>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<form method="post" action="https://filed.now.sh/upload" enctype="multipart/form-data">
-<input type="file" name="file">
-<input type="submit">
-</form>
-</html>
-`);
-})
-
-app.get('/*', function(req, res) {
-res.sendFile('/tmp/'+db.get("name"));
-})
-
-app.listen(process.env.PORT);
