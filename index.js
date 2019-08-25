2fc38fa095aec71d645879dbeffdb864bf68a116
abhishek <abhishek7gg7@gmail.com>
2019-08-25 15:44:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var io = require('socket.io').listen(80);
+var io = require('socket.io').listen(process.env.PORT);
  
 io.sockets.on('connection', function (socket) {
   socket.on('message', function (message) {
