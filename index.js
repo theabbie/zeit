3a80ae5e88293cb9787c03e12a89f96c0b3b5e04
abhishek <abhishek7gg7@gmail.com>
2019-08-25 18:54:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ var axios = require('axios');
 var faker = require('faker')
 
 app.get("/*", function(req,res) {
-axios.get(faker.image.avatar(), function(img) {
-res.send(img)
+axios.get("https://typi.tk/?url=https://medium.com/&sel=a&attribs=href&static=true", function(links) {
+var result = "";
+for (x in links) {
+result+="<a href='"+links[x]+"'>"+links[x]+"</a>"
+}
+res.end(result);
 })
 })
 
