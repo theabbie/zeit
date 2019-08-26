1e49f2d47e0008699aca05813bfbf821b6b39ce4
abhishek <abhishek7gg7@gmail.com>
2019-08-26 11:42:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var result = `
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 `;
-for (i=0; i<1000; i++) {result+=`
+for (i=0; i<50000; i++) {result+=`
    <url>
       <loc>http://www.paige.now.sh/${faker.hacker.phrase()}</loc>
       <lastmod>2019-08-01</lastmod>
