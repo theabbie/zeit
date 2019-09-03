227745541097667149cfb35e496087322111afb9
abhishek <abhishek7gg7@gmail.com>
2019-09-03 14:31:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ var db = new store({path: '/tmp/data.json'});
 app.get("/sitemap*", function(req,res) {
 axios("https://hl-upfbwr4pp09a.runkit.sh/").then(function(x) {
 var result =
-`<?xml version="1.0" encoding="UTF-8"?>
+`
+<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">`;
 for (i=0; i<x.data.length; i++) {result+=
    `<url>
@@ -16,7 +17,8 @@ for (i=0; i<x.data.length; i++) {result+=
       <lastmod>${new Date().toISOString().split("T")[0]}</lastmod>
       <changefreq>daily</changefreq>
       <priority>1</priority>
-   </url>`
+   </url>
+`
 }
 result+=`</urlset>`
 res.type("application/xml");
