930a49011992cfe9cd742c0bc7fcbd6c1307b2d6
abhishek <abhishek7gg7@gmail.com>
2019-09-06 13:30:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -218,6 +218,32 @@ ${req.url.split("?s=")[0]=="/"?"":(db.has(decodeURIComponent(req.url.substring(1
 <h3 class="w3-container w3-padding-32">${req.query.s?((topics.includes(req.query.s.toLowerCase())?"Search":"No")+" results for "+req.query.s.bold()):""}</h3>
 <div class="w3-row">
 <div class="w3-col l8 s12">
+${(function() {
+var rs = ``;
+for (i=0; i<10; i++) {
+rs+=
+`<div class="w3-card-4 w3-margin w3-white">
+    <img src="https://source.unsplash.com/800x450/?${faker.random.words().split(" ").join(",").split("-").join(",")}" alt="Nature" style="width:100%">
+    <div class="w3-container">
+      <h3><b><a href="/${phrase[i]}">${phrase[i]}</a></b></h3>
+      <h5>${faker.name.firstName()}, <span class="w3-opacity">September 2, 2019</span></h5>
+    </div>
+    <div class="w3-container">
+      <p>${phrase[i]+"<br>#AI #Machine Learning <br> #Universe #Technology <br> #Adsense #Programming <br> #SEO"}</p>
+      <div class="w3-row">
+        <div class="w3-col m8 s12">
+        </div>
+        <div class="w3-col m4 w3-hide-small">
+          <p><span class="w3-padding-large w3-right"><b>Comments &nbsp;</b> <span class="w3-tag">0</span></span></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <hr>\n`
+}
+return rs;
+})()}
+<!--
   <div class="w3-card-4 w3-margin w3-white">
     <img src="https://source.unsplash.com/800x450/?${faker.random.words().split(" ").join(",").split("-").join(",")}" alt="Nature" style="width:100%">
     <div class="w3-container">
@@ -398,6 +424,7 @@ ${req.url.split("?s=")[0]=="/"?"":(db.has(decodeURIComponent(req.url.substring(1
     </div>
   </div>
   <hr>
+-->
 <!-- END BLOG ENTRIES -->
 </div>
 <!-- Introduction menu -->
