4e0e22df6c07d627c59a4978599605b2584cb3ce
abhishek <abhishek7gg7@gmail.com>
2019-09-07 13:23:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -232,7 +232,7 @@ rs+=
       <h5>${faker.name.firstName()}, <span class="w3-opacity">${faker.date.past().toString().split(" ").splice(0,4).join(" ")}</span></h5>
     </div>
     <div class="w3-container">
-      <p>${phrase[i]+"<br>#AI #Machine Learning #Universe<br> #Technology #Adsense #Programming <br> #SEO #database #cloud}</p>
+      <p>${phrase[i]+"<br>#AI #Machine Learning #Universe<br> #Technology #Adsense #Programming <br> #SEO #database #cloud"}</p>
       <div class="w3-row">
         <div class="w3-col m8 s12">
         </div>
