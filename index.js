28919dd0f386d44f85a1a0165667fd5c0a68ece2
abhishek <abhishek7gg7@gmail.com>
2019-09-11 21:06:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var faker = require('faker');
 var store = require('data-store');
 const $ = require("cheerio");
 var db = new store({path: '/tmp/data.json'});
-var topics = ['technology','universe','ai','machine learning','programming','adsense','seo','database','cloud','youtube','wikipedia'];
-var tags = "<br>#AI #Machine Learning #Universe<br> #Technology #Adsense #Programming<br> #SEO #database #cloud<br> #youtube #wikipedia";
+var topics = ['technology','universe','ai','machine learning','programming','adsense','seo','database','cloud','youtube','wikipedia','fcrit'];
+var tags = "<br>#AI #Machine Learning #Universe #Technology<br> #Adsense #Programming #SEO #database<br> #cloud #youtube #wikipedia #fcrit";
 
 app.get("/logo", function(req,res) {res.redirect(301,"https://cdn.jsdelivr.net/gh/theabbie/awto@gh-pages/files/IMG_20190720_184556.jpg")});
 
