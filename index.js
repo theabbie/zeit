0c6ca8dccf109389dc63e0f725712b2556cda5f4
abhishek <abhishek7gg7@gmail.com>
2020-01-19 15:32:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ var result =
 for (i=0; i<hls.length; i++) {result+=
 `<item>
   <title>${hls[i].split("&").join("&amp;").split('"').join("&quot;").split("'").join("&apos;").trim()}</title>
-  <link>https://${req.headers.host}/${hls[i].split("&").join("&amp;").split('"').join("&quot;").split("'").join("&apos;").trim().split(" ").join("%20")}</link>
+  <link>https://${req.headers.host}/${hls[i].split("&").join("&amp;").split('"').join("&quot;").split("'").join("&apos;").trim()}</link>
   <description>${hls[i].split("&").join("&amp;").split('"').join("&quot;").split("'").join("&apos;").trim()}</description>
 </item>
 `
