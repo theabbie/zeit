31fef3603a0fda2f21dd8e664cbc32a25fcf0425
abhishek <abhishek7gg7@gmail.com>
2020-03-07 10:46:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -171,17 +171,6 @@ var result =
   gtag('js', new Date());
   gtag('config', 'UA-99338607-8');
 </script>
-<!-- Hotjar Tracking Code for https://iferr.now.sh -->
-<script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:1696747,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
 <script type="application/ld+json">
 {
    "@context":"https://schema.org",
