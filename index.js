d3d7b943c64045e568fa4fe399245bd66ac60a0e
abhishek <abhishek7gg7@gmail.com>
2020-04-10 10:42:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1729,6 +1729,7 @@ var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
 <script>
 document.querySelector("header").onclick=function() {location.href="https://google.com/search?q=${decodeURIComponent(req.url.substring(1))}"}
 </script>
+<script type="text/javascript"> var infolinks_pid = 3248588; var infolinks_wsid = 0; </script> <script type="text/javascript" src="//resources.infolinks.com/js/infolinks_main.js"></script>
 </html>`;
 res.type("text/html").end(result);
 }))
